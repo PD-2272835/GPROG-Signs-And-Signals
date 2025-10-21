a1b5a01260f15f1a5214d45ecd9c0ccff0767db3
PD-2272835 <up2272835@myport.ac.uk>
2025-10-21 02:24:56 +0100

    Grid Code Cleanup/Testing
    ~Grid.cs Code cleanup, organized into sections (will try to keep this coding style across all other classes)
    +Testing.cs just as a quick way to make sure the grid and debug grid representation is working as intended
    
    Regenerated project, hence build settings and shadergraph settings are updated with whitespace - repo re-cloned, all working properly now on laptop

--- Assets/MapGenerator/Grid.cs
+++ Assets/MapGenerator/Grid.cs
@@ -1,7 +1,8 @@
 //Adapted from Code Monkey on Youtube (2019) https://www.youtube.com/watch?v=8jrAWtI8RXg
+using System.Buffers;
 using UnityEngine;
 
-public class Grid<TGridObject>
+public class CustomGrid<TGridObject>
 {
     private int _width = 1;
     private int _height = 1;
@@ -10,24 +11,46 @@ public class Grid<TGridObject>
 
     private TGridObject[,] _cells;
 
-    public Grid(int gridWidth, int gridHeight, float cellSize, Vector3 originPosition)
+    public CustomGrid(int gridWidth, int gridHeight, float cellSize, Vector3 originPosition)
     {
         _width = gridWidth;
         _height = gridHeight;
         _cellSize = cellSize;
         _originPosition = originPosition;
         _cells = new TGridObject[gridWidth, gridHeight];
+
+        DebugDrawGrid();
+    }
+
+    //Helper Functions
+    public void DebugDrawGrid()
+    {
+        float maxWorldspaceGridHeight = _originPosition.y + (_height * _cellSize);
+        float worldSpaceGridWidth = _originPosition.x + (_width * _cellSize);
+        for (int x = 0; x < _width + 1; x++)
+        {
+            Debug.DrawLine(new Vector3(_originPosition.x + x, _originPosition.y, _originPosition.z),
+                            new Vector3(_originPosition.x + x, maxWorldspaceGridHeight, _originPosition.z),
+                            Color.white, 100f);
+        }
+
+        for (int y = 0; y < _height + 1; y++)
+        {
+            Debug.DrawLine(new Vector3(_originPosition.x, _originPosition.y + y, _originPosition.z),
+                            new Vector3(worldSpaceGridWidth, _originPosition.y + y, _originPosition.z),
+                            Color.white, 100f);
+            Debug.Log("meow");
+        }
     }
 
     public bool IsInGrid(int x, int y)
     {
-        if (x >= 0 && y >= 0 && x <= _width && y <= _height)
-        {
-            return true;
-        }
+        if (x >= 0 && y >= 0 && x <= _width && y <= _height) return true;
         return false;
     }
 
+
+    //Setters
     public void SetCellValue(TGridObject newValue, int x, int y)
     {
         if (IsInGrid(x, y))
@@ -36,19 +59,21 @@ public class Grid<TGridObject>
         }
     }
 
+
+    //Getters
     public TGridObject GetCellValue(int x, int y)
     {
         if (IsInGrid(x, y))
         {
             return _cells[x, y];
         }
-        return default;
+        return default(TGridObject);
     }
 
     public TGridObject GetCellValue(Vector3 worldPosition)
     {
-        int x = Mathf.FloorToInt((worldPosition.x / _cellSize) - _originPosition.x);
-        int y = Mathf.FloorToInt((worldPosition.y / _cellSize) - _originPosition.y);
+        int x = Mathf.FloorToInt((worldPosition - _originPosition).x / _cellSize);
+        int y = Mathf.FloorToInt((worldPosition - _originPosition).y / _cellSize);
         return GetCellValue(x, y);
     }
 }
---
+++ Assets/MapGenerator/Testing.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Testing : MonoBehaviour
+{
+    public CustomGrid<bool> balls;
+    [SerializeField] private int _gridWidth = 10, _gridHeight = 10;
+    InputAction action;
+
+
+    void Start()
+    {
+        balls = new CustomGrid<bool>(_gridWidth, _gridHeight, 1f, transform.position);
+        action = InputSystem.actions.FindAction("Interact");
+    }
+
+   
+    void Update()
+    {
+        if (action.IsPressed())
+        {
+            balls.DebugDrawGrid();
+        }
+    }
+}
