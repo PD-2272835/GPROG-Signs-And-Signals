c46c35cf23519d12788e0b309b00dcc8ae45ec40
PD-2272835 <186196474+PD-2272835@users.noreply.github.com>
2025-12-10 18:01:04 +0000

    Input Handling for player
    +Basic Birds Eye Camera Movement
    +Click callback that returns collided object (will use this for card actor object selection)

---
+++ Assets/Player/GameplayInputHandler.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class GameplayInputHandler : MonoBehaviour
+{
+    private Camera _mainCam;
+
+    private Vector2 _moveDirection;
+    public float movementSpeed = 10.0f;
+    
+
+    public void Awake()
+    {
+        _mainCam = Camera.main;
+    }
+
+    private void Update()
+    {
+        Vector2 modifiedMovementDirection = _moveDirection * movementSpeed * Time.deltaTime;
+        _mainCam.transform.position += new Vector3(modifiedMovementDirection.x, modifiedMovementDirection.y, 0);
+    }
+
+
+    public void OnLeftClick(InputAction.CallbackContext context)
+    {
+        if (!context.started) return;
+        
+        var rayHit = Physics2D.GetRayIntersection(_mainCam.ScreenPointToRay(Mouse.current.position.ReadValue()));
+    }
+
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        _moveDirection = context.ReadValue<Vector2>().normalized;
+    }
+}
